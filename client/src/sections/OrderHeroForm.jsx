69b1f34538aabf12b31dcfcd3cc8870070bdfa7b
mjmandelah07 <aramidemojisolagrace@gmail.com>
2024-05-16 00:03:06 +0100

    Hero form done

--- client/src/sections/OrderHeroForm.jsx
+++ client/src/sections/OrderHeroForm.jsx
@@ -1,21 +1,52 @@
-import { useState } from "react";
-import Select from "react-select";
+import * as React from "react";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import ListItemText from "@mui/material/ListItemText";
+import Select from "@mui/material/Select";
+import Checkbox from "@mui/material/Checkbox";
+import TextField from "@mui/material/TextField";
 import "../assets/styles/order-hero.css";
 import { Container, Row, Col } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
-import ValueContainer from "../components/CustomSelectComponents.js";
 
-  const options = [
-    { value: "chocolate", label: "Chocolate" },
-    { value: "strawberry", label: "Strawberry" },
-    { value: "vanilla", label: "Vanilla" },
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+const names = [
+  "Oliver Hansen",
+  "Van Henry",
+  "April Tucker",
+  "Ralph Hubbard",
+  "Omar Alexander",
+  "Carlos Abbott",
+  "Miriam Wagner",
+  "Bradley Wilkerson",
+  "Virginia Andrews",
+  "Kelly Snyder",
 ];
 
-
-  
 export default function OrderHeroForm() {
-  const [selectedOption, setSelectedOption] = useState(null);
+  const [personName, setPersonName] = React.useState([]);
 
+  const handleChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setPersonName(
+      // On autofill we get a stringified value.
+      typeof value === "string" ? value.split(",") : value
+    );
+  };
   return (
     <div className="section-reservation">
       <Container>
@@ -25,82 +56,127 @@ export default function OrderHeroForm() {
               <Form className="d-block d-lg-flex">
                 <div className="fields d-block d-lg-flex">
                   <Col xs={12} md={2} className="textfield-name one-third">
-                    <Form.Group
-                      className="mb-lg-0 mb-3"
-                      controlId="exampleForm.ControlInput1"
-                    >
-                      <Form.Label>Email address</Form.Label>
-                      <Form.Control
-                        size="lg"
-                        type="text"
-                        id="name"
-                        name="name"
-                        value=""
-                        placeholder="Name"
-                      />
-                    </Form.Group>
+                    <TextField
+                      id="outlined-basic"
+                      label="Outlined"
+                      variant="outlined"
+                      className="form-control"
+                      sx={{
+                        width: "100%",
+                        "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline":
+                          {
+                            borderColor: "#404044",
+                            borderWidth: "1px",
+                          },
+                      }}
+                    />
                   </Col>
                   <Col xs={12} md={2} className="one-third textfield-phone">
-                    <Form.Group
-                      className="mb-lg-0 mb-3"
-                      controlId="exampleForm.ControlInput1"
-                    >
-                      <Form.Label>Email address</Form.Label>
-                      <Form.Control
-                        size="lg"
-                        type="text"
-                        id="name"
-                        name="name"
-                        value=""
-                        placeholder="Name"
-                      />
-                    </Form.Group>
+                    <TextField
+                      id="outlined-basic"
+                      label="Outlined"
+                      variant="outlined"
+                      className="form-control"
+                      sx={{
+                        width: "100%",
+                        borderRadius: 0,
+                        "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline":
+                          {
+                            borderColor: "#404044",
+                            borderWidth: "1px",
+                          },
+                        "& .MuiOutlinedInput-root": {
+                          borderRadius: "0",
+                        },
+                      }}
+                    />
                   </Col>
                   <Col xs={12} md={2} className="one-third">
-                    <Form.Group
-                      className="mb-lg-0 mb-3"
-                      controlId="exampleForm.ControlInput1"
-                    >
-                      <Form.Label>Email address</Form.Label>
-                      <Form.Control
-                        size="lg"
-                        type="text"
-                        id="name"
-                        name="name"
-                        value=""
-                        placeholder="Name"
-                      />
-                    </Form.Group>
+                    <TextField
+                      id="outlined-basic"
+                      label="Outlined"
+                      variant="outlined"
+                      className="form-control"
+                      sx={{
+                        width: "100%",
+                        borderRadius: 0,
+                        "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline":
+                          {
+                            borderColor: "#404044",
+                            borderWidth: "1px",
+                          },
+                        "& .MuiOutlinedInput-root": {
+                          borderRadius: "0",
+                        },
+                      }}
+                    />
                   </Col>
                   <Col xs={12} md={2} className="one-third">
-                    <Form.Group
-                      className="mb-lg-0 mb-3"
-                      controlId="exampleForm.ControlInput1"
-                    >
-                      <Form.Label>Email address</Form.Label>
-                      <Form.Control
-                        size="lg"
-                        type="text"
-                        id="name"
-                        name="name"
-                        value=""
-                        placeholder="Name"
-                      />
-                    </Form.Group>
+                    <TextField
+                      id="outlined-basic"
+                      label="Outlined"
+                      variant="outlined"
+                      className="form-control"
+                      sx={{
+                        width: "100%",
+                        borderRadius: 0,
+                        "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline":
+                          {
+                            borderColor: "#404044",
+                            borderWidth: "1px",
+                          },
+                        "& .MuiOutlinedInput-root": {
+                          borderRadius: "0",
+                        },
+                      }}
+                    />
                   </Col>
                   <Col xs={12} md={2} className="one-third select-wrap">
-                    <Form.Label>Email address</Form.Label>
-                    <Select
-                      name="food"
-                      defaultValue={selectedOption}
-                      onChange={setSelectedOption}
-                      options={options}
-                      isMulti
-                      components={{ ValueContainer }}
-                      placeholder="Select food"
-                      // closeMenuOnSelect={false}
-                      hideSelectedOptions={false}
-                    />
+                    <FormControl
+                      sx={{
+                        width: "100%",
+                        "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline":
+                          {
+                            borderColor: "#404044",
+                            borderWidth: "1px",
+                          },
+                        "& .MuiOutlinedInput-root": {
+                          borderRadius: "0",
+                        },
+                      }}
+                    >
+                      <InputLabel id="demo-multiple-checkbox-label">
+                        Select Food
+                      </InputLabel>
+                      <Select
+                        labelId="demo-multiple-checkbox-label"
+                        id="demo-multiple-checkbox"
+                        multiple
+                        value={personName}
+                        onChange={handleChange}
+                        input={<OutlinedInput label="Select Food" />}
+                        renderValue={(selected) => selected.join(", ")}
+                        MenuProps={MenuProps}
+                        sx={{
+                          width: "100%",
+                        }}
+                      >
+                        {names.map((name) => (
+                          <MenuItem key={name} value={name}>
+                            <Checkbox
+                              checked={personName.indexOf(name) > -1}
+                              sx={{
+                                "&.Mui-checked, &.MuiCheckbox-indeterminate": {
+                                  color: "#f24405",
+                                },
+                              }}
+                            />
+                            <ListItemText primary={name} />
+                          </MenuItem>
+                        ))}
+                      </Select>
+                    </FormControl>
+                    ;
                   </Col>
                 </div>
                 <input
